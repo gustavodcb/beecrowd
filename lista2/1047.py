443124f593cd60e54121b75deafb2c3a7a185e47
Gustavo Davi <gustavodavi.gdcb@gmail.com>
2024-03-15 01:34:12 -0300

    update files

--- lista2/1047.py
+++ lista2/1047.py
@@ -1,13 +1,17 @@
-HoraInicial, MinutosInicial, HorasFinais, MinutosFinais = map(int, input().split())
+hora_inicio, minuto_inicio, hora_fim, minuto_fim = map(int, input().split())
 
-if (HoraInicial < HorasFinais and MinutosInicial < MinutosFinais) or (HoraInicial == HorasFinais and MinutosInicial > MinutosFinais):
-    DuracaoHoras = HoraInicial - HorasFinais
-    DuracaoMinutos = MinutosInicial - MinutosFinais
-elif (HoraInicial > HorasFinais and MinutosInicial > MinutosFinais or (HoraInicial > HorasFinais and MinutosInicial < MinutosFinais)):
-    DuracaoHoras = 24 - HoraInicial + HorasFinais
-    DuracaoMinutos = 60 - MinutosInicial + MinutosFinais
+inicio_em_minutos = hora_inicio * 60 + minuto_inicio
+fim_em_minutos = hora_fim * 60 + minuto_fim
+
+if fim_em_minutos <= inicio_em_minutos:
+    fim_em_minutos += 24 * 60
+
+duracao_total_minutos = fim_em_minutos - inicio_em_minutos
+
+horas = duracao_total_minutos // 60
+minutos = duracao_total_minutos % 60
+
+if horas == 24 and minutos == 0:
+    print("O JOGO DUROU 24 HORA(S) E 0 MINUTO(S)")
 else:
-    DuracaoHoras = 24
-    DuracaoMinutos = 0
-
-print(f"O JOGO DUROU {DuracaoHoras} HORA(S) E {DuracaoMinutos} MINUTO(S)")
+    print(f"O JOGO DUROU {horas} HORA(S) E {minutos} MINUTO(S)")
---
+++ lista2/2006.py
@@ -0,0 +1,2 @@
+usuario = int(input())
+A, B, C, D = map(int(input()))
